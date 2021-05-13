7119f9ace5a52d59bbc0e90733183ed4043a7c4a
triana <trianaheinz@gmail.com>
2021-05-13 16:30:18 +0200

    working on stripe and nodemailer

--- models/User.model.js
+++ models/User.model.js
@@ -12,6 +12,7 @@ const userSchema = new Schema(
   password: {type: String, required: true},
   name: {type: String, required: true},
   surname: {type: String, required: true},
+  linkedinID: {type: String},
   phone: {type: Number, required: true},
   position: {type: String},
   products: {type:Schema.Types.ObjectId, ref:'Product'},

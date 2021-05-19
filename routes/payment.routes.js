d08cd9b1074977ae27b59f2dbd35a3fb966e1a4d
triana <trianaheinz@gmail.com>
2021-05-19 12:36:49 +0200

    updates

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -5,33 +5,48 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const Product = require("./../models/Product.model");
 const User = require("./../models/User.model");
 
-paymentRouter.post("/create-checkout-session/:id", async (req, res) => {
+paymentRouter.post('/reserve/:id', async (req, res) => {
+
   const product = await Product.findById(req.params.id);
+
   const { id } = req.user;
+
   const updatedUser = await User.findOneAndUpdate(
     { _id: id },
     {$push: {'products': product._id}},
     {new: true}
-    );
-  const objToPurchase = {
-    price_data: {
-      currency: "eur",
-      product_data: {
-        name: product.name,
-      },
-    },
-    quantity: 1,
-  };
+    )
 
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    line_items: [objToPurchase],
-    mode: "payment",
-    success_url: `http://localhost:3000/userhomepage`,
-    cancel_url: `http://localhost:3000/`,
-  });
+    res.status(200).json(updatedUser)
+})
 
-  res.json({ id: session.id });
-});
+// paymentRouter.post("/create-checkout-session/:id", async (req, res) => {
+//   const product = await Product.findById(req.params.id);
+//   const { id } = req.user;
+//   const updatedUser = await User.findOneAndUpdate(
+//     { _id: id },
+//     {$push: {'products': product._id}},
+//     {new: true}
+//     );
+//   const objToPurchase = {
+//     price_data: {
+//       currency: "eur",
+//       product_data: {
+//         name: product.name,
+//       },
+//     },
+//     quantity: 1,
+//   };
+
+//   const session = await stripe.checkout.sessions.create({
+//     payment_method_types: ["card"],
+//     line_items: [objToPurchase],
+//     mode: "payment",
+//     success_url: `http://localhost:3000/userhomepage`,
+//     cancel_url: `http://localhost:3000/`,
+//   });
+
+//   res.json({ id: session.id });
+// });
 
 module.exports = paymentRouter;
